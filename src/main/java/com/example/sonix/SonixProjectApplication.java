56e159568219a2c0dc151639e77065313dfcbc90
akuzmik <Hodzyvy6!>
2024-10-11 17:28:34 +0200

    init commit

---
+++ src/main/java/com/example/sonix/SonixProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonixProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SonixProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/sonix/SonixProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonixProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
