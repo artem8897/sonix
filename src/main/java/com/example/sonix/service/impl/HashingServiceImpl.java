8a2fdfcc7be8d61434021c1d2df381b3db653e56
akuzmik <Hodzyvy6!>
2024-10-13 23:21:31 +0200

    deploy

--- src/main/java/com/example/sonix/service/impl/HashingServiceImpl.java
+++ src/main/java/com/example/sonix/service/impl/HashingServiceImpl.java
@@ -12,12 +12,14 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class HashingServiceImpl implements HashingService {
 
